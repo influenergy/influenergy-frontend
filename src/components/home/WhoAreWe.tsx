efabbb8dca1be75f9b8f3b1771f022cd9e377446
Kumar Satwik Satamanyu Beura <kumarsatwik6@gmail.com>
2025-03-11 13:22:21 +0530

    feat: Replace image with video in WhoAreWe component for enhanced engagement

--- src/components/home/WhoAreWe.tsx
+++ src/components/home/WhoAreWe.tsx
@@ -7,21 +7,24 @@ import Link from "next/link";
 
 export default function WhoAreWe() {
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 justify-center w-full px-6 md:px-16 lg:px-24 py-20 md:py-28 gap-10 items-center max-w-[1440px] mx-auto">
+    <div className="grid grid-cols-1 md:grid-cols-2 justify-around w-full px-6 md:px-16 lg:px-24 py-10 md:py-10 gap-10 items-center max-w-[1440px] mx-auto">
       <motion.div
-        className="flex justify-center md:justify-end"
+        className="flex justify-center  h-full"
         initial={{ opacity: 0, x: -40 }}
         whileInView={{ opacity: 1, x: 0 }}
         transition={{ duration: 0.7 }}
         viewport={{ once: true, amount: 0.3 }}
       >
-        <Image
-          src="/landing/whoarewe.webp"
-          alt="Who we are"
-          width={500}
-          height={500}
-          className="rounded-3xl object-cover shadow-lg"
-        />
+        <video
+          autoPlay
+          loop
+          muted
+          playsInline
+          className="rounded-3xl object-cover shadow-lg h-[550px]"
+          controls
+        >
+          <source src="/landing/hearfromus/1.mov" />
+        </video>
       </motion.div>
 
       <motion.div
