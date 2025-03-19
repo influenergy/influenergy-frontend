2a0a1744de97de521f1c0d7801f0b3a5cedf31f4
Kumar Satwik Satamanyu Beura <kumarsatwik6@gmail.com>
2025-03-19 11:07:47 +0530

    feat: Add new icons and manifest file for improved web app support

--- src/components/dashboard/Sidebar.tsx
+++ src/components/dashboard/Sidebar.tsx
@@ -38,12 +38,7 @@ export default function Sidebar({ type }: { type: string }) {
     <div className="border-r bg-primary md:block fixed left-0 top-0 h-screen w-16 md:w-[240px] z-10 transition-all duration-300">
       <div className="flex h-full flex-col gap-2 p-4">
         <Link href="/dashboard">
-          <Image
-            src="/images/logo-white.svg"
-            width={200}
-            height={200}
-            alt="logo"
-          />
+          <Image src="/images/logo-white.svg" width={200} height={200} alt="logo" />
         </Link>
         <hr />
         <div className="w-full flex flex-col items-center justify-center">
@@ -61,68 +56,29 @@ export default function Sidebar({ type }: { type: string }) {
         </div>
 
         <nav className="grid items-start gap-2 mt-2">
-          {navItems.map((item: NavItem) =>
-            item.children ? (
-              // Nested navigation
-              <Collapsible
-                key={item.label}
-                open={openItem === item.label}
-                onOpenChange={() =>
-                  setOpenItem(openItem === item.label ? null : item.label)
-                }
+          {navItems.map((item: NavItem) => (
+            <Link key={item.href} href={item.href!}>
+              <Button
+                variant="ghost"
+                className={cn(
+                  "w-full justify-start gap-3 hover:text-primary",
+                  pathname === item.href
+                    ? "bg-white text-primary"
+                    : "text-white"
+                )}
               >
-                <CollapsibleTrigger asChild>
-                  <Button
-                    variant="ghost"
-                    className="w-full justify-start gap-3"
-                  >
-                    {item.icon && (
-                      <item.icon className="h-4 w-4 hover:text-primary" />
-                    )}
-                    <p className="text-sm md:block hidden text-primary">
-                      {item.label}
-                    </p>
-                    <ChevronsUpDown className="h-4 w-4 ml-7 md:block hidden" />
-                  </Button>
-                </CollapsibleTrigger>
-                <CollapsibleContent className="pl-8">
-                  {item.children.map((child) => (
-                    <Link key={child.href} href={child.href}>
-                      <Button
-                        variant="ghost"
-                        className={cn(
-                          "w-full justify-start text-sm",
-                          pathname === child.href && "bg-white text-primary"
-                        )}
-                      >
-                        {child.icon && <child.icon className="h-4 w-4" />}
-                        <p className="text-sm">{child.label}</p>
-                      </Button>
-                    </Link>
-                  ))}
-                </CollapsibleContent>
-              </Collapsible>
-            ) : (
-              // Simple navigation
-              <Link key={item.href} href={item.href!}>
-                <Button
-                  variant="ghost"
-                  className={cn(
-                    "w-full justify-start gap-3",
-                    pathname === item.href && "bg-white text-primary"
-                  )}
-                >
-                  {item.icon && <item.icon className="h-4 w-4" />}
-                  <p className="text-sm md:block hidden">{item.label}</p>
-                </Button>
-              </Link>
-            )
-          )}
+                {item.icon && <item.icon className="h-4 w-4" />}
+                <p className="text-sm md:block hidden ">{item.label}</p>
+              </Button>
+            </Link>
+          ))}
         </nav>
 
         {/* Settings button */}
         <div className="mt-auto ">
-        <p className="text-sm text-center text-white md:block hidden">© influenergy</p>
+          <p className="text-sm text-center text-white md:block hidden">
+            © influenergy
+          </p>
         </div>
       </div>
     </div>
