b8b8d9db6f05de4ddfa1ae8de9573fca4254cebd
Kumar Satwik Satamanyu Beura <kumarsatwik6@gmail.com>
2025-03-19 22:43:44 +0530

    feat: Remove MobileSidebar component from Header for cleaner layout

--- src/components/dashboard/Header.tsx
+++ src/components/dashboard/Header.tsx
@@ -1,10 +1,8 @@
-import { MobileSidebar } from "./MobileSidebar";
 import { UserNav } from "./UserNav";
 
 export default function Header() {
   return (
     <div className="flex h-16 items-center px-4 border-b bg-white">
-      <MobileSidebar />
       <div className="ml-auto flex items-center space-x-4">
         <UserNav />
       </div>
