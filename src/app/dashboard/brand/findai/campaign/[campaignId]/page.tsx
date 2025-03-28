8ee85234817eabffddff0bdc23f536e18e90de2a
Kumar Satwik Satamanyu Beura <kumarsatwik6@gmail.com>
2025-03-28 11:33:13 +0530

    feat(campaign): add back button and creator details enhancements
    - Added a back button to navigate to the previous page.
    - Improved creator card display with category, follower count, and platform links.
    - Enhanced the display of creator names and follower counts.
    - Added a separator between primary and secondary social links.
    - Improved the button style.

--- src/app/dashboard/brand/findai/campaign/[campaignId]/page.tsx
+++ src/app/dashboard/brand/findai/campaign/[campaignId]/page.tsx
@@ -4,12 +4,13 @@ import { Button } from "@/components/ui/button";
 import { MdVerified } from "react-icons/md";
 import Link from "next/link";
 import { useFindAiCampaign } from "@/hooks/useFindAi";
-import { Loader2 } from "lucide-react";
+import { ChevronsLeft, Loader2 } from "lucide-react";
 import { CreatorAPIResponse } from "@/types/Creator";
-import { useParams } from "next/navigation";
+import { useParams, useRouter } from "next/navigation";
 
 export default function ProfileMatch() {
   const { campaignId } = useParams();
+  const router = useRouter();
   const {
     data: creatorsData,
     isLoading,
@@ -43,9 +44,17 @@ export default function ProfileMatch() {
       </div>
     );
   }
-
   return (
     <div className="container mx-auto mt-8 px-5">
+      <button
+        type="button"
+        onClick={() => router.back()}
+        className="text-gray-600 hover:text-gray-900 transition-colors duration-200 flex items-center gap-1 mb-5"
+      >
+        <ChevronsLeft className="h-5 w-5" />
+        Back
+      </button>
+
       <h2 className="text-2xl font-bold mb-4">Creators</h2>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
         {creators?.map((data: CreatorAPIResponse) => (
@@ -54,7 +63,7 @@ export default function ProfileMatch() {
             className="bg-white rounded-lg shadow-md overflow-hidden flex flex-col"
           >
             <Image
-              src={data?.profileIcon || "/images/login.webp"}
+              src={data?.creator?.profileIcon || "/images/login.webp"}
               alt={data?.creator?.fullName}
               width={500}
               height={500}
@@ -63,24 +72,31 @@ export default function ProfileMatch() {
             <div className="flex flex-col p-4 flex-grow">
               <div className="flex items-start justify-between mb-2">
                 <div className="flex flex-col justify-start items-start  w-full">
-                  <p className="text-lg font-semibold flex items-center gap-3">
-                    {data?.creator?.fullName}{" "}
+                  <p className="text-lg font-semibold flex items-center gap-1">
+                    {data?.creator?.fullName?.length > 10
+                      ? `${data.creator.fullName.slice(0, 10)}...`
+                      : data.creator.fullName}
                     <MdVerified className=" text-blue-500 h-5 w-5" />
                   </p>
                   <p className="text-gray-600 text-sm text-start ">
-                    {data?.creator?.profile?.category?.join(", ")}
+                    {`${data?.creator?.profile?.category?.[0]?.slice(0, 20)}${
+                      data?.creator?.profile?.category?.[0] &&
+                      data?.creator?.profile?.category?.[0].length > 10
+                        ? "..."
+                        : ""
+                    }`}
                   </p>{" "}
                 </div>
 
                 <div className="flex flex-col items-end  w-full ">
-                  <p className="text-gray-800 font-semibold text-right text-xs">
+                  <p className="text-gray-800 font-semibold text-right text-sm">
                     {data?.creator?.profile?.socialLinks?.primary?.followers}
                   </p>
-                  <p>Followers</p>
+                  <p className="text-sm">Followers</p>
                 </div>
               </div>
               <div className="h-0.5 bg-gray-200 w-full" />
-              <div className="flex items-center mt-2 space-x-4">
+              <div className="flex items-center mt-2 space-x-2">
                 {data?.creator?.profile?.socialLinks?.primary?.platform && (
                   <Link
                     className="text-xs text-primary"
@@ -91,6 +107,10 @@ export default function ProfileMatch() {
                     {data?.creator?.profile?.socialLinks?.primary?.platform}
                   </Link>
                 )}
+                {data?.creator?.profile?.socialLinks?.primary?.platform &&
+                  data?.creator?.profile?.socialLinks?.secondary?.platform && (
+                    <span className="text-gray-300">|</span>
+                  )}
                 {data?.creator?.profile?.socialLinks?.secondary?.platform && (
                   <Link
                     className="text-xs text-primary"
@@ -103,15 +123,15 @@ export default function ProfileMatch() {
                   </Link>
                 )}
               </div>
+            </div>
+
+            <div className="p-4 pt-0 mt-auto">
               <div className="mt-4 flex justify-between items-center">
                 <p className="text-sm text-gray-700">Price Per Video</p>
                 <p className="text-lg font-bold">
                   $ {data?.creator?.profile?.budgetVideo}
                 </p>
               </div>
-            </div>
-
-            <div className="p-4 pt-0 mt-auto">
               <Link
                 href={`/dashboard/brand/findai/campaign/${campaignId}/creator/${
                   data?.creatorId
@@ -119,7 +139,7 @@ export default function ProfileMatch() {
               >
                 <Button
                   variant="outline"
-                  className="w-full bg-primary text-white hover:text-primary hover:bg-white"
+                  className="w-full bg-primary text-white border border-primary hover:text-primary hover:bg-white "
                 >
                   View Details
                 </Button>
