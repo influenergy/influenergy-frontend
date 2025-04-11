220f1240fe22078cc18721dcf096ab3450d3138c
Kumar Satwik Satamanyu Beura <kumarsatwik6@gmail.com>
2025-04-11 14:31:22 +0530

    fix(Header): wrap logo image in a link for better navigation

--- src/components/home/Header.tsx
+++ src/components/home/Header.tsx
@@ -6,7 +6,9 @@ export default function Header() {
   return (
     <>
       <header className="flex justify-between items-center w-full h-24 px-10 shadow-md  z-10">
-        <Image src="/images/logo.svg" alt="" width={200} height={200} />
+        <Link href="/">
+          <Image src="/images/logo.svg" alt="" width={200} height={200} />
+        </Link>
         <div className="flex justify-center items-center gap-3">
           <Link href="/get-started">
             <Button className="bg-primary text-white rounded-xl px-9 py-5">
