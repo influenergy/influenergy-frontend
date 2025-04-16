b606323c9ce39138b33908d481fb4e4ccefdc169
Parshant Rao <parshant@onelabventures.com>
2025-04-16 12:30:34 +0530

    added t&c doc link

--- src/components/auth/RegisterForm.tsx
+++ src/components/auth/RegisterForm.tsx
@@ -212,19 +212,24 @@ export default function RegisterForm({ userType }: { userType: string }) {
 
                 <p className="text-xs sm:text-sm font-light">
                   I accept all{" "}
-                  <Link
-                    href="/terms"
-                    className="underline text-primary hover:text-[#6564d8] transition-colors"
-                  >
-                    terms of use
-                  </Link>{" "}
-                  and{" "}
-                  <Link
-                    href="/privacy"
-                    className="underline text-primary hover:text-[#6564d8] transition-colors"
-                  >
-                    privacy policy
-                  </Link>
+                  <a
+                      
+                      href="/Influenergy - Terms of Service.pdf"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="underline text-primary hover:text-[#6564d8] transition-colors"
+                    >
+                      terms of use
+                    </a>{" "}
+                    and{" "}
+                    <a
+                      href="/Influenergy - Privacy Policy.pdf"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="underline text-primary hover:text-[#6564d8] transition-colors"
+                    >
+                      privacy policy
+                    </a>
                 </p>
               </motion.div>
               {errors.terms && (
