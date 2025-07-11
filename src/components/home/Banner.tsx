d811a7be6907f6c6dc7374d99aa2eb250511656f
Parshant Rao <parshant@onelabventures.com>
2025-07-11 10:55:43 +0530

    Refactor Banner component for improved styling and responsiveness: Updated layout for better alignment, adjusted text properties for clarity, and removed unused code to streamline the component.

--- src/components/home/Banner.tsx
+++ src/components/home/Banner.tsx
@@ -1,32 +1,25 @@
 'use client';
 
 import React from 'react';
-// import Link from 'next/link';
 
 function Banner() {
-
   return (
-    <div className="w-full bg-green-300 border-b border-green-400 overflow-hidden py-2 flex justify-between content-center items-center">
+    <div className="w-full bg-green-300 border-b border-green-400 py-2 px-4 flex items-center justify-center overflow-x-auto">
       <div
-        className="flex items-center whitespace-nowrap text-center text-sm font-medium text-red-600 justify-center content-center w-full"
-        // animate={{ x: ["100%", "-100%"] }}
-        // transition={{ repeat: Infinity, ease: "linear", duration: 50 }}
+        className="
+          text-center 
+          text-sm 
+          md:text-base 
+          font-medium 
+          text-red-600 
+          animate-none 
+          md:animate-marquee
+          whitespace-normal 
+          md:whitespace-nowrap
+        "
       >
-        {/* repeat this span for smooth loop */}
-        <span className="inline-block text-lg text-center ">
-          Unleash your creativity with Filmora’s intuitive video editor—seamless, powerful, and tailored for influencers and brands &nbsp; 
-          <a
-            className="underline"
-            href="https://filmora.wondershare.com/?utm_source=instagram&utm_medium=banner&utm_campaign=partnership&utm_term=jeev&utm_content=image_21111744_2025-04-23"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-             TRY NOW.
-          </a>
-
-        </span>
-        {/* <span className="inline-block text-lg ">
-          Unleash your creativity with Filmora’s intuitive video editor—seamless, powerful, and tailored for influencers and brands &nbsp; 
+        <span className="inline-block">
+          Unleash your creativity with Filmora’s intuitive video editor — seamless, powerful, and tailored for influencers and brands.&nbsp;
           <a
             className="underline"
             href="https://filmora.wondershare.com/?utm_source=instagram&utm_medium=banner&utm_campaign=partnership&utm_term=jeev&utm_content=image_21111744_2025-04-23"
@@ -35,11 +28,9 @@ function Banner() {
           >
             TRY NOW.
           </a>
-
-        </span> */}
+        </span>
       </div>
     </div>
-
   );
 }
 
