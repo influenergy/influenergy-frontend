eaf1e59760d232169778bfa44455410cd3dc8a7c
Parshant Rao <parshant@onelabventures.com>
2025-07-11 10:11:53 +0530

    Update Banner component with new content and styles: Changed background color and border styles for better visibility, replaced the animated text with a static promotional message for Filmora's video editor, and added a link for users to try the product.

--- src/components/home/Banner.tsx
+++ src/components/home/Banner.tsx
@@ -1,28 +1,45 @@
 'use client';
 
 import React from 'react';
-import { motion } from 'framer-motion';
 // import Link from 'next/link';
 
 function Banner() {
 
   return (
-    <div className="w-full bg-green-100 border-b border-green-200 overflow-hidden py-2">
-    <motion.div
-      className="flex whitespace-nowrap text-center text-sm font-medium text-red-600 gap-32 justify-between"
-      animate={{ x: ["100%", "-100%"] }}
-      transition={{ repeat: Infinity, ease: "linear", duration: 50 }}
-    >
-      {/* repeat this span for smooth loop */}
-      <span className="inline-block text-lg ">
-        🔥 New! OM KOP Brand onboarded. <span className="underline">Login to learn more.</span>
-      </span>
-      <span className="inline-block text-lg ">
-        🔥 New! OM KOP Brand onboarded. <span className="underline">Login to learn more.</span>
-      </span>
-    </motion.div>
-  </div>
-  
+    <div className="w-full bg-green-300 border-b border-green-400 overflow-hidden py-2 flex justify-between content-center items-center">
+      <div
+        className="flex items-center whitespace-nowrap text-center text-sm font-medium text-red-600 justify-center content-center w-full"
+        // animate={{ x: ["100%", "-100%"] }}
+        // transition={{ repeat: Infinity, ease: "linear", duration: 50 }}
+      >
+        {/* repeat this span for smooth loop */}
+        <span className="inline-block text-lg text-center ">
+          Unleash your creativity with Filmora’s intuitive video editor—seamless, powerful, and tailored for influencers and brands &nbsp; 
+          <a
+            className="underline"
+            href="https://filmora.wondershare.com/?utm_source=instagram&utm_medium=banner&utm_campaign=partnership&utm_term=jeev&utm_content=image_21111744_2025-04-23"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+             TRY NOW.
+          </a>
+
+        </span>
+        {/* <span className="inline-block text-lg ">
+          Unleash your creativity with Filmora’s intuitive video editor—seamless, powerful, and tailored for influencers and brands &nbsp; 
+          <a
+            className="underline"
+            href="https://filmora.wondershare.com/?utm_source=instagram&utm_medium=banner&utm_campaign=partnership&utm_term=jeev&utm_content=image_21111744_2025-04-23"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            TRY NOW.
+          </a>
+
+        </span> */}
+      </div>
+    </div>
+
   );
 }
 
